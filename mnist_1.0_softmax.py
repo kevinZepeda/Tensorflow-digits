40ce1bb2f034c9c8b62e2b520a0d8a3ab3c3785f
alex <alexmaguey1@gmail.com>
2018-02-13 17:07:18 -0600

    Add imports and seed

---
+++ mnist_1.0_softmax.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+import tensorflowvisu
+from tensorflow.examples.tutorials.mnist import input_data as mnist_data
+print("Tensorflow version " + tf.__version__)
+tf.set_random_seed(0)
