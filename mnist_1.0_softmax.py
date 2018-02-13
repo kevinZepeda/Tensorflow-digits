704ced6492264efd1ff9afd0ac8968d9ca19f21d
alex <alexmaguey1@gmail.com>
2018-02-13 17:48:03 -0600

    Done :D

--- mnist_1.0_softmax.py
+++ mnist_1.0_softmax.py
@@ -47,3 +47,30 @@ datavis = tensorflowvisu.MnistDataVis()
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
+
+# Funcion para entrenar el modelo 100 imagenes a la vez
+def training_step(i, update_test_data, update_train_data):
+
+    # entrenar en batches de 100 imagenes con 100 labels
+    batch_X, batch_Y = mnist.train.next_batch(100)
+
+    # computar valores de entrenamiento para visualizacion
+    if update_train_data:
+        a, c, im, w, b = sess.run([accuracy, cross_entropy, I, allweights, allbiases], feed_dict={X: batch_X, Y_: batch_Y})
+        datavis.append_training_curves_data(i, a, c)
+        datavis.append_data_histograms(i, w, b)
+        datavis.update_image1(im)
+        print(str(i) + ": accuracy:" + str(a) + " loss: " + str(c))
+
+    # computar valores de prueba para visualizacion
+    if update_test_data:
+        a, c, im = sess.run([accuracy, cross_entropy, It], feed_dict={X: mnist.test.images, Y_: mnist.test.labels})
+        datavis.append_test_curves_data(i, a, c)
+        datavis.update_image2(im)
+        print(str(i) + ": ********* epoch " + str(i*100//mnist.train.images.shape[0]+1) + " ********* test accuracy:" + str(a) + " test loss: " + str(c))
+
+    # propagacion del entrenamiento
+    sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y})
+
+# animacion
+datavis.animate(training_step, iterations=2000+1, train_data_update_freq=10, test_data_update_freq=50, more_tests_at_start=True)
